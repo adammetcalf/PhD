442b5a743fe8626f85da89b87e230bece3c5dad3
adammetcalf <adamgilesmetcalf@gmail.com>
2018-10-23 16:58:16 +0100

    did some work

--- Matlab/DynamicModel.m
+++ Matlab/DynamicModel.m
@@ -11,6 +11,15 @@ T1_deg = 90; % degrees.
 T0_rad = degtorad(T0_deg); % radians.
 T1_rad = degtorad(T1_deg); % radians.
 
+%Initialise end effector Jacobian (J_ee_theta)(should probably make this a
+%function, since it will be called every iteration of the loop).
+J_ee_theta = [-L0*sin(T0_rad)-L1*sin(T0_rad+T1_rad) -L1*sin(T0_rad+T1_rad)
+              L0*cos(T0_rad)+L1*cos(T0_rad+T1_rad) +L1*cos(T0_rad+T1_rad)
+              0 0
+              0 0
+              0 0
+              1 1];
+
 %Perform Forward Kinematics.
 [X0,X1,X2] = FKin(L0,L1,T0_rad,T1_rad);
 
@@ -22,30 +31,53 @@ linkdata on
 
 % Begin State Machine loop.
 Stop = false; %set stop tag to false.
+state = "getuserinput"; %set 1st state to get the desired coordinates
 while Stop == false
 
-    if state==getuserinput
+    if state=="getuserinput"
     %Build vector X2des, which is the desired x and y of the end effector
         
-        %Next state = Inverse Kinematics
-        state = IK;
+        %Get the desired x-coordinate
+        x2des = input('what is the desired x coordinate?');
         
-    elseif state ==Ik
+        %Get the desired y-coordinate
+        y2des = input('what is the desired y coordinate?');
+        
+        %Build the desired end effector vector
+        X2des = [x2des, y2des];    
+    
+        %Next state = Inverse Kinematics
+        state = "IK";
+        
+    elseif state =="IK"
     %Perform Inverse Kinematics to turn X2des into Desired Angles.
         
+        %Perform Forward Kinematics.
+        [T0_des, T1_des] = IKin(L0,L1,X2des);
+        
         %Next state = Forward Kinematics
-        state = FK;
+        state = "FK";
         
-    elseif state==FK
+    elseif state=="FK"
     %Perform Forward kinematics to obtain current positions.
+    
+        %Perform Forward Kinematics.
+        [X0,X1,X2] = FKin(L0,L1,T0_rad,T1_rad);
         
-    elseif state ==control
+        %Next state = Control
+        state = "control";
+        
+    elseif state =="control"
     %Implement control algorithm
     
-    elseif state == display
-    %Update chart
+        %Next state = display
+        state = "display";
         
-    elseif state == finish
+    elseif state == "display"
+    %Update chart
+    [x,y] = cartesian(X0,X1,X2);
+        
+    elseif state == "finish"
     % change stop tag to true to stop loop
     Stop = True;
     
--- Matlab/IKin.m
+++ Matlab/IKin.m
@@ -5,12 +5,12 @@ function [T0_des, T1_des] = IKin (L0,L1,X2des)
 
 L = sqrt((X2des(1))^2+(X2des(2))^2);
 
-Alpha = acos((L^2 + L0^2 + L1^2)/(2*L*L0));
+Alpha = acos((L^2 + L0^2 - L1^2)/(2*L*L0));
 
 Beta = atan(X2des(2)/X2des(1));
 
 T0_des = Beta-Alpha;
 
-Zeta = acos((L^2 + L0^2 - L^2)/(2*L0*L1));
+Zeta = acos((L0^2 + L1^2 - L^2)/(2*L0*L1));
 
 T1_des = degtorad(180) - Zeta;
